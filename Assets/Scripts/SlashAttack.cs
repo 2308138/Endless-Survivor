5e36ec25901896079a5e7ca3fd66a3b5e41e7ee5
2308138 <2308138@student.toa.edu.my>
2025-04-27 15:12:45 +0800

    u3
    - Added Slash Attack

---
+++ Assets/Scripts/SlashAttack.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class SlashAttack : MonoBehaviour
+{
+    public float lifetime = 0.2F;
+
+    private void Start()
+    {
+        Destroy(gameObject, lifetime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
